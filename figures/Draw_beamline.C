26ddde91dc9ab41c1308afbe4a9ceb0138859289
Elton Smith <elton@jlab.org>
2019-10-10 14:59:21 -0400

    updated beamline figure with correct origin

--- figures/Draw_beamline.C
+++ figures/Draw_beamline.C
@@ -19,8 +19,10 @@ void Draw_beamline()
     float xbeam1= 218;
     
     float xoffset = xbeam1;    // distances from beamline.
-    float zoffset = 300;
-    float zscale=9904./861;
+    float zorigin=9904.1;
+    float zscale=zorigin/861;
+    float zoffset = 300 + zorigin/zscale;
+    // float zoffset = 300;
     float xscale=zscale;
     
     // invert xpts
@@ -52,8 +54,9 @@ void Draw_beamline()
     
       // float zmin = 0;
       // float zmax=3200;
-      float zmin = -3000;
-      float zmax=14000;
+    float zorigin_approx = 10000;
+      float zmin = -3000 - zorigin_approx;
+      float zmax=14000 - zorigin_approx;
       float xmin= -1500;
       float xmax=1500;
       float fudge=240./181.;
@@ -104,10 +107,11 @@ void Draw_beamline()
     tagger->Draw("f");
     
     float goniz = (300-zoffset)*zscale;
+    float Dxgoni = 100;
     
-    TMarker *goniometer = new TMarker(goniz,xbeam,2);
-    goniometer->SetMarkerColor(2);
-    goniometer->SetMarkerSize(2);
+    TLine *goniometer = new TLine(goniz,Dxgoni/2,goniz,-Dxgoni/2);
+    goniometer->SetLineWidth(4);
+    goniometer->SetLineColor(kRed);
     goniometer->Draw();
     
     TMarker *taggerMid = new TMarker(ztag,xbeam,2);
@@ -130,7 +134,7 @@ void Draw_beamline()
      // modify relative to original offsets
     for (Int_t jj=0;jj<Npoints;jj++) {
         xc1[jj] = (xoffset - 400)*xscale + xc1[jj];
-        // zc1[jj] = zc1[jj]  + (300 - zoffset)*zscale;
+        zc1[jj] = zc1[jj] - zorigin;
         cout << " jj=" << jj << " xc1=" << xc1[jj] << " zc1=" << zc1[jj] << endl;
     }
     
@@ -213,17 +217,25 @@ void Draw_beamline()
          float Dxcol=20*4;
          float zc5[]={zcol-Dzcol/2,zcol+Dzcol/2,zcol+Dzcol/2,zcol-Dzcol/2,zcol-Dzcol/2};
          float xc5[]={0,0,Dxcol,Dxcol,0};
+    
+    for (Int_t jj=0; jj< Npoints; jj++) {
+        zc5[jj] = zc5[jj] - zorigin;
+    }
         
          TPolyLine *collimator = new TPolyLine(Npoints, zc5, xc5);
-         collimator->SetLineWidth(1.0);
+         collimator->SetLineWidth(2.0);
          collimator->SetLineColor(kRed);
          collimator->Draw("l");
     
      float zc6[]={zcol-Dzcol/2,zcol+Dzcol/2,zcol+Dzcol/2,zcol-Dzcol/2,zcol-Dzcol/2};
      float xc6[]={0,0,-Dxcol,-Dxcol,0};
+     
+     for (Int_t jj=0; jj< Npoints; jj++) {
+         zc6[jj] = zc6[jj] - zorigin;
+     }
     
      TPolyLine *collimator2 = new TPolyLine(Npoints, zc6, xc6);
-     collimator2->SetLineWidth(1.0);
+     collimator2->SetLineWidth(2.0);
      collimator2->SetLineColor(kRed);
      collimator2->Draw("l");
     
@@ -233,11 +245,30 @@ void Draw_beamline()
       float DxTpol=20*4;
       float zc7[]={zTpol-DzTpol/2,zTpol+DzTpol/2,zTpol+DzTpol/2,zTpol-DzTpol/2,zTpol-DzTpol/2};
       float xc7[]={-DxTpol/2,-DxTpol/2,DxTpol/2,DxTpol/2,-DxTpol/2};
-     
+      
+      for (Int_t jj=0; jj< Npoints; jj++) {
+          zc7[jj] = zc7[jj] - zorigin;
+      }
+    
       TPolyLine *Tpol = new TPolyLine(Npoints, zc7, xc7);
-      Tpol->SetLineWidth(1.0);
+      Tpol->SetLineWidth(2.0);
       Tpol->SetLineColor(kRed);
       Tpol->Draw("l");
+    
+      Npoints=5;
+      float z5C11B=-570;
+      float Dz5C11B=20*4;
+      float Dx5C11B=20*4;
+      float zc7a[]={z5C11B-Dz5C11B/2,z5C11B+Dz5C11B/2,z5C11B+Dz5C11B/2,z5C11B-Dz5C11B/2,z5C11B-Dz5C11B/2};
+      float xc7a[]={-Dx5C11B/2,-Dx5C11B/2,Dx5C11B/2,Dx5C11B/2,-Dx5C11B/2};
+      
+      for (Int_t jj=0; jj< Npoints; jj++) {
+          zc7a[jj] = zc7a[jj] - zorigin;
+      }
+      TPolyLine *b5C11B = new TPolyLine(Npoints, zc7a, xc7a);
+      b5C11B->SetLineWidth(2.0);
+      b5C11B->SetLineColor(kRed);
+      b5C11B->Draw("l");
     
        Npoints=5;
        float zPS=8766.9;
@@ -245,9 +276,12 @@ void Draw_beamline()
        float DxPS=45*4;
        float zc8[]={zPS-DzPS/2,zPS+DzPS/2,zPS+DzPS/2,zPS-DzPS/2,zPS-DzPS/2};
        float xc8[]={-DxPS/2,-DxPS/2,DxPS/2,DxPS/2,-DxPS/2};
-      
+       
+       for (Int_t jj=0; jj< Npoints; jj++) {
+           zc8[jj] = zc8[jj] - zorigin;
+       }
        TPolyLine *PS = new TPolyLine(Npoints, zc8, xc8);
-       PS->SetLineWidth(1.0);
+       PS->SetLineWidth(2.0);
        PS->SetLineColor(kRed);
        PS->Draw("l");
        
@@ -257,13 +291,17 @@ void Draw_beamline()
           float zsolenoid=Dzsolenoid/2-50.8+9904.1;
           float zc9[]={zsolenoid-Dzsolenoid/2,zsolenoid+Dzsolenoid/2,zsolenoid+Dzsolenoid/2,zsolenoid-Dzsolenoid/2,zsolenoid-Dzsolenoid/2};
           float xc9[]={-Dxsolenoid/2,-Dxsolenoid/2,Dxsolenoid/2,Dxsolenoid/2,-Dxsolenoid/2};
-         
+          
+          for (Int_t jj=0; jj< Npoints; jj++) {
+              zc9[jj] = zc9[jj] - zorigin;
+          }
+    
           TPolyLine *solenoid = new TPolyLine(Npoints, zc9, xc9);
-          solenoid->SetLineWidth(1.0);
+          solenoid->SetLineWidth(2.0);
           solenoid->SetLineColor(kRed);
           solenoid->Draw("l");
     
-          float ztarget = 65. + 9904.1;
+          float ztarget = 65. + 9904.1 - zorigin;
     
           TMarker *target = new TMarker(ztarget,xbeam,2);
           target->SetMarkerColor(2);
@@ -276,9 +314,12 @@ void Draw_beamline()
              float zgdump=11644+Dzgdump/2;
              float zc10[]={zgdump-Dzgdump/2,zgdump+Dzgdump/2,zgdump+Dzgdump/2,zgdump-Dzgdump/2,zgdump-Dzgdump/2};
              float xc10[]={-Dxgdump/2,-Dxgdump/2,Dxgdump/2,Dxgdump/2,-Dxgdump/2};
-            
+             
+             for (Int_t jj=0; jj< Npoints; jj++) {
+                 zc10[jj] = zc10[jj] - zorigin;
+             }
              TPolyLine *gdump = new TPolyLine(Npoints, zc10, xc10);
-             gdump->SetLineWidth(1.0);
+             gdump->SetLineWidth(2.0);
              gdump->SetLineColor(kBlack);
     gdump->Draw("l");
     // dump1->SetFillStyle(3545);
@@ -292,15 +333,41 @@ void Draw_beamline()
              float zcolwall=8542;
              float zc10a[]={zcolwall-Dzcolwall/2,zcolwall+Dzcolwall/2,zcolwall+Dzcolwall/2,zcolwall-Dzcolwall/2,zcolwall-Dzcolwall/2};
              float xc10a[]={-Dxcolwall/2,-Dxcolwall/2,Dxcolwall/2,Dxcolwall/2,-Dxcolwall/2};
-            
+             
+             for (Int_t jj=0; jj< Npoints; jj++) {
+                 zc10a[jj] = zc10a[jj] - zorigin;
+             }
              TPolyLine *colwall = new TPolyLine(Npoints, zc10a, xc10a);
-             colwall->SetLineWidth(1.0);
+             colwall->SetLineWidth(2.0);
              colwall->SetLineColor(kBlack);
     colwall->Draw("l");
     // dump1->SetFillStyle(3545);
     colwall->SetFillStyle(3005);
     colwall->SetFillColor(kBlack);
     colwall->Draw("f");
+    
+    
+    
+    Npoints=5;
+    float Dzaccwall=150;
+    float Dxaccwall=300;
+    float Dxaccwall2=50;
+    float zaccwall=-1460;
+    float zc10b[]={zaccwall-Dzaccwall/2,zaccwall+Dzaccwall/2,zaccwall+Dzaccwall/2,zaccwall-Dzaccwall/2,zaccwall-Dzaccwall/2};
+    float xc10b[]={-Dxaccwall,-Dxaccwall,Dxaccwall2,Dxaccwall2,-Dxaccwall};
+    
+    for (Int_t jj=0; jj< Npoints; jj++) {
+        zc10b[jj] = zc10b[jj] - zorigin;
+    }
+    TPolyLine *accwall = new TPolyLine(Npoints, zc10b, xc10b);
+    accwall->SetLineWidth(2.0);
+    accwall->SetLineColor(kBlack);
+    accwall->Draw("l");
+    // dump1->SetFillStyle(3545);
+    accwall->SetFillStyle(3005);
+    accwall->SetFillColor(kBlack);
+    accwall->Draw("f");
+    
     
        Npoints=5;
        float Dztdipole=659;
@@ -324,112 +391,133 @@ void Draw_beamline()
         zc11[4] = zc11[0];
         xc11[4] = xc11[0];
     
-    
        TPolyLine *tdipole = new TPolyLine(Npoints, zc11, xc11);
        tdipole->SetLineWidth(2.0);
        tdipole->SetLineColor(kRed);
        tdipole->Draw("l");
     
-    TLine *plane = new TLine(363,-62,1178,-163);
+    TLine *plane = new TLine(363-zorigin,-62,1178-zorigin,-163);
     plane->SetLineWidth(3.0);
     plane->SetLineColor(kRed);
     plane->Draw("l");
     
     float Dzdash = 125;
     float Dxdash = 100;
-    TLine *arrow = new TLine(-214,510,-55,123);
+    TLine *arrow = new TLine(-214-zorigin,510,-55-zorigin,123);
     arrow->SetLineWidth(2);
     arrow->SetLineColor(kBlack);
     arrow->Draw();
-    TLine *dash = new TLine(-214,510,-214-Dzdash,510);
+    TLine *dash = new TLine(-214-zorigin,510,-214-zorigin-Dzdash,510);
     dash->SetLineWidth(2);
     dash->SetLineColor(kBlack);
     dash->Draw();
     
     TString string;
     string.Form("radiator");
-    TLatex *t3 = new TLatex(-214-11*Dzdash,510-Dxdash,string);
+    TLatex *t3 = new TLatex(-214-zorigin-11*Dzdash,510-Dxdash,string);
     t3->SetTextSize(0.07);
     t3->Draw();
     
     Dzdash = 125;
     Dxdash = 100;
-    arrow->DrawLine(374,846,634,123);
-    dash->DrawLine(374,846,374-Dzdash,846);
+    arrow->DrawLine(374-zorigin,846,634-zorigin,123);
+    dash->DrawLine(374-zorigin,846,374-zorigin-Dzdash,846);
     string.Form("tagging magnet");
-    t3->DrawLatex(374-20*Dzdash,846-Dxdash,string);
+    t3->DrawLatex(374-zorigin-20*Dzdash,846-Dxdash,string);
     
     Dzdash = 125;
     Dxdash = 100;
-    arrow->DrawLine(7333,485,7525,156);
-    dash->DrawLine(7333,485,7333-Dzdash,485);
+    arrow->DrawLine(7333-zorigin,485,7525-zorigin,156);
+    dash->DrawLine(7333-zorigin,485,7333-zorigin-Dzdash,485);
     string.Form("collimator");
-    t3->DrawLatex(7333-13*Dzdash,485-Dxdash,string);
+    t3->DrawLatex(7333-zorigin-13*Dzdash,485-Dxdash,string);
     
     Dzdash = 125;
     Dxdash = 100;
-    arrow->DrawLine(7955,813,8306,115);
-    dash->DrawLine(7955,813,7955-Dzdash,813);
+    arrow->DrawLine(7955-zorigin,813,8306-zorigin,115);
+    dash->DrawLine(7955-zorigin,813,7955-zorigin-Dzdash,813);
     string.Form("triple polarimeter");
-    t3->DrawLatex(7955-22*Dzdash,813-Dxdash,string);
+    t3->DrawLatex(7955-zorigin-22*Dzdash,813-Dxdash,string);
     
     Dzdash = 125;
     Dxdash = 100;
-    arrow->DrawLine(8429,1183,8759,174);
-    dash->DrawLine(8429,1183,8429-Dzdash,1183);
+    arrow->DrawLine(8429-zorigin,1183,8759-zorigin,174);
+    dash->DrawLine(8429-zorigin,1183,8429-zorigin-Dzdash,1183);
     string.Form("pair spectrometer");
-    t3->DrawLatex(8429-22.5*Dzdash,1183-Dxdash,string);
+    t3->DrawLatex(8429-zorigin-22.5*Dzdash,1183-Dxdash,string);
     
     string.Form("solenoid");
     // t3->DrawLatex(9324,-425,string);
     Dzdash = 125;
     Dxdash = 100;
-    arrow->DrawLine(11791,561,10400,89);
-    dash->DrawLine(11791,561,11791+Dzdash,561);
-    t3->DrawLatex(11791+2*Dzdash,561-Dxdash,string);
+    arrow->DrawLine(11791-zorigin,561,10400-zorigin,89);
+    dash->DrawLine(11791-zorigin_approx,561,11791-zorigin_approx+Dzdash,561);
+    t3->DrawLatex(11791-zorigin_approx+2*Dzdash,561-Dxdash,string);
     
     Dzdash = 125;
     Dxdash = 100;
-    arrow->DrawLine(10320,898,9958,123);
-    dash->DrawLine(10320,898,10320+Dzdash,898);
+    arrow->DrawLine(10320-zorigin,898,9958-zorigin,123);
+    dash->DrawLine(10320-zorigin,898,10320-zorigin+Dzdash,898);
     string.Form("target center");
-    t3->DrawLatex(10320+2*Dzdash,898-Dxdash/2,string);
+    t3->DrawLatex(10320-zorigin+2*Dzdash,898-Dxdash/2,string);
     
     Dzdash = 125;
     Dxdash = 100;
-    arrow->DrawLine(3606,-845,2950,-660);
-    dash->DrawLine(3606,-845,3606+Dzdash,-845);
+    arrow->DrawLine(3606-zorigin,-845,2950-zorigin,-660);
+    dash->DrawLine(3606-zorigin,-845,3606-zorigin+Dzdash,-845);
     string.Form("electron dump");
-    t3->DrawLatex(3606+2*Dzdash,-845-Dxdash,string);
+    t3->DrawLatex(3606-zorigin+2*Dzdash,-845-Dxdash,string);
     
     Dzdash = 125;
     Dxdash = 100;
-    arrow->DrawLine(11929,-597,11748,0);
-    dash->DrawLine(11929,-597,11929+Dzdash,-597);
+    arrow->DrawLine(11929-zorigin,-597,11748-zorigin,0);
+    dash->DrawLine(11929-zorigin,-597,11929-zorigin+Dzdash,-597);
     string.Form("photon");
-    t3->DrawLatex(11929+2*Dzdash,-597-Dxdash,string);
+    t3->DrawLatex(11929-zorigin+2*Dzdash,-597-Dxdash,string);
     string.Form("dump");
-    t3->DrawLatex(11929+5*Dzdash,-597-4*Dxdash,string);
+    t3->DrawLatex(11929-zorigin+5*Dzdash,-597-4*Dxdash,string);
     
     Dzdash = 125;
     Dxdash = 100;
-    arrow->DrawLine(3968,696,4132,0);
-    dash->DrawLine(3968,696,3968-Dzdash,696);
+    arrow->DrawLine(3968-zorigin,696,4132-zorigin,0);
+    dash->DrawLine(3968-zorigin,696,3968-zorigin-Dzdash,696);
     string.Form("photon beam");
-    t3->DrawLatex(3368-12*Dzdash,696-Dxdash,string);
+    t3->DrawLatex(3368-zorigin-12*Dzdash,696-Dxdash,string);
     
     Dzdash = 125;
     Dxdash = 100;
-    arrow->DrawLine(-2490,-719,-1845,0);
-    dash->DrawLine(-2490,-719,-2490+Dzdash,-719);
+    arrow->DrawLine(-2490-zorigin,-1000,-1845-zorigin,0);
+    dash->DrawLine(-2490-zorigin,-1000,-2490-zorigin+Dzdash,-1000);
     string.Form("electron beam");
-    t3->DrawLatex(-2490+2*Dzdash,-719-Dxdash,string);
+    t3->DrawLatex(-2490-zorigin+2*Dzdash,-1000-Dxdash,string);
     
-    string.Form("tagger building");
-    t3->DrawLatex(-500,-1248,string);
+    Dzdash = 125;
+    Dxdash = 100;
+    arrow->DrawLine(-1160-zorigin,-668,-570-zorigin,-79);
+    dash->DrawLine(-1160-zorigin,-668,-1160-zorigin+Dzdash,-668);
+    string.Form("5C11B");
+    TLatex *t4 = new TLatex(-1160-zorigin+2*Dzdash,-668-Dxdash,string);
+    t4->SetTextSize(0.05);
+    t4->Draw();
+    
+    TLine *bAD00 = new TLine(2269-zorigin,-348,2220-zorigin,-441);
+    bAD00->SetLineWidth(4);
+    bAD00->SetLineColor(kRed);
+    bAD00->Draw();
+    
+    Dzdash = 125;
+    Dxdash = 100;
+    arrow->DrawLine(-6817,-1291,-7697,-492);
+    dash->DrawLine(-6817,-1291,-6817+Dzdash,-1291);
+    string.Form("AD00");
+    t4->DrawLatex(-6817+1*Dzdash,-1291-Dxdash,string);
+    
+    
+    string.Form("Tagger Hall");
+    t3->DrawLatex(500-zorigin,-1248,string);
     
     string.Form("Hall D");
-    t3->DrawLatex(9618,-1248,string);
+    t3->DrawLatex(9618-zorigin,-1248,string);
     
     /*t3->DrawLatex(zfilter-2,toffset,"filter");
     t3->DrawLatex(zdiffuser-2,toffset,"diffuser");
